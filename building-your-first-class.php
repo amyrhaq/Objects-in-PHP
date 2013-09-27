c863fa602cba51b658a12dd7489ce2b0e96f18ba
Your Name Here <amyr@amyrhaq.com>
2013-09-26 20:02:01 -0700

    completed lesson 3/10

---
+++ building-your-first-class.php
@@ -0,0 +1,18 @@
+<!-- Add a pair of php tags. Create a Person class.
+Create two instances of the Person class: $teacher and $student. -->
+<html>
+	<head>
+	  <title>Building Your First Class</title>
+	</head>
+	<body>
+      <p>
+		<?php
+		class Person {
+
+		}
+		$teacher = new Person();
+		$student = new Person();
+		?>      
+ 	  </p>
+    </body>
+</html>
