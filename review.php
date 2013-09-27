a8689e0a9da502769bed13d83a134d18dce8bb82
Your Name Here <amyr@amyrhaq.com>
2013-09-26 21:50:13 -0700

    completed lesson 10/10

---
+++ review.php
@@ -0,0 +1,9 @@
+<!-- echo the following sentence: "Now I know the basics of OOP!". -->
+<html>
+<head>
+	<title>Review</title>
+</head>
+<body>
+<p><?php echo "Now I know the basics of OOP!" ?></p>
+</body>
+</html>
